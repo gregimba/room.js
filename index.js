58739e49af566decc57fe1226bd904d2b323cbd4
Grant Regimbal <regimbal.grant@gmail.com>
2018-03-09 14:43:27 -0600

    use promise & get sunset

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-var weather = require('openweather-apis');
+let weather = require('openweather-apis');
 const config = require('./config')
 
+//intial api config
 weather.setLang('en');
 weather.setZipCode(78701);
 weather.setUnits('imperial');
 weather.setAPPID(config.openweather);
 
 
-weather.getAllWeather((err, JSONObj) => {
-  if(err) console.log(err);
-  else console.log(JSONObj);
-});
+
+function getWeather() {
+  return new Promise((resolve,reject) => {
+    weather.getAllWeather((err, JSONObj) => {
+      resolve(JSONObj);
+
+      reject(err);
+    });
+  });
+}
+
+function getSunValues(value) {
+  console.log("Sunrise => " + new Date(value.sys.sunrise * 1000))
+  console.log("sunset => " + new Date(value.sys.sunset * 1000))
+}
+
+getWeather().then((currentWeather) => getSunValues(currentWeather));
