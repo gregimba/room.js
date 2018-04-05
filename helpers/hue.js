02cee350b0f72dab78632de9d86442e598f13048
Grant Regimbal <regimbal.grant@gmail.com>
2018-04-04 22:40:40 -0500

    more hue helpers

--- helpers/hue.js
+++ helpers/hue.js
@@ -8,4 +8,65 @@ const client = new huejay.Client({
   timeout: 15000
 });
 
+toggle = which => {
+  client.groups
+    .getById(1)
+    .then(group => {
+      group.on = which;
+      return client.groups.save(group);
+    })
+    .then(group => {})
+    .catch(error => {
+      console.log(error.stack);
+    });
+};
+
+rainbow = () => {
+  client.groups
+    .getById(1)
+    .then(group => {
+      console.log(`Saving group...`);
+      group.on = true;
+      group.effect = 'colorloop';
+      group.brightness = 50;
+
+      return client.groups.save(group);
+    })
+    .then(group => {
+      console.log('New hue:', group.hue);
+      console.log('New saturation:', group.saturation);
+    })
+    .catch(error => {
+      console.log(error.stack);
+    });
+};
+
+lightStatus = callback => {
+  client.groups.getById(1).then(group => {
+    callback(group.action.attributes);
+  });
+};
+
+setLights = color => {
+  client.groups
+    .getById(1)
+    .then(group => {
+      group.on = true;
+      group.brightness = color.brightness; //0-254
+      group.hue = color.hue; //0 to 65535
+      group.saturation = color.saturation; //0-254
+      group.colorTemp = color.colorTemp; //153 to 500
+
+      return client.groups.save(group);
+    })
+    .then(group => {})
+    .catch(error => {
+      console.log(error.stack);
+    });
+};
+
+module.exports.setLights = setLights;
+module.exports.lightStatus = lightStatus;
+module.exports.rainbow = rainbow;
 module.exports.hue = client;
+module.exports.toggle = toggle;
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { getWeather } = require('./helpers/weather');
 
-const { hue } = require('./helpers/hue');
+const hue = require('./helpers/hue');
 
-getWeather()
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+hue.rainbow();
