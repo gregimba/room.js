d4667f8688b876c2e746abfab8e1b9dc2144776e
Grant Regimbal <regimbal.grant@gmail.com>
2018-04-05 18:53:29 -0500

    yay webpack

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const { getWeather } = require('./helpers/weather');
 const hue = require('./helpers/hue');
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use('/', express.static(path.join(__dirname, './client/dist')));
 app.get('/rainbow', (req, res) => {
   hue.rainbow();
   res.send('sit back and relax!');
@@ -19,6 +20,11 @@ app.post('/status', (req, res) => {
   hue.setLights(req.body);
   res.status(201);
   res.json({ done: true });
+});
+
+app.post('/toggle', (req, res) => {
+  hue.toggle(req.body.toggle);
+  res.json({ toggle: req.body.toggle });
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
