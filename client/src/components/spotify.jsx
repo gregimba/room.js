f53bf2dbb9d02f04bc2122c3484ec8e723ec8a67
Grant Regimbal <regimbal.grant@gmail.com>
2018-04-06 18:50:04 -0500

    move spotify over into a component

--- client/src/components/spotify.jsx
+++ client/src/components/spotify.jsx
@@ -1,5 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Welcome from './welcome.jsx';
+
 class Spotify extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { spotify: false };
+    this.reloadCurrentSpotify = this.reloadCurrentSpotify.bind(this);
+  }
+  componentDidMount() {
+    this.reloadCurrentSpotify();
+  }
+
+  reloadCurrentSpotify() {
+    axios
+      .get('/spotify')
+      .then(body => {
+        let next_call_ms =
+          body.data.item.duration_ms - body.data.progress_ms + 100;
+        let timeout = window.setTimeout(
+          this.reloadCurrentSpotify,
+          next_call_ms
+        );
+        this.setState({ spotify: body });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    if (this.state.spotify !== false) {
+      const songArtists = this.state.spotify.data.item.artists.map(artist => (
+        <li>{artist.name}</li>
+      ));
+      const songName = this.state.spotify.data.item.name;
+      return (
+        <div>
+          <h1>{songName}</h1>
+          <ul>{songArtists}</ul>
+        </div>
+      );
+    } else {
+      return <h1> Loading spotify </h1>;
+    }
   }
 }
+
+export default Spotify;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,46 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import Welcome from './components/welcome.jsx';
+import Spotify from './components/spotify.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { spotify: false };
-    this.reloadCurrentSpotify = this.reloadCurrentSpotify.bind(this);
-  }
-  componentDidMount() {
-    this.reloadCurrentSpotify();
-  }
-
-  reloadCurrentSpotify() {
-    axios
-      .get('/spotify')
-      .then(body => {
-        let next_call_ms = (body.data.item.duration_ms - body.data.progress_ms + 100);
-        console.log(next_call_ms);
-        let timeout = window.setTimeout(this.reloadCurrentSpotify, next_call_ms);
-        this.setState({ spotify: body });
-      })
-      .catch(error => {
-        console.log(error);
-      });
   }
 
   render() {
-    if (this.state.spotify !== false) {
-      const songArtists = this.state.spotify.data.item.artists.map(artist => (
-        <li>{artist.name}</li>
-      ));
-      const songName = this.state.spotify.data.item.name;
-      return (
-        <div>
-          <h1>{songName}</h1>
-          <ul>{songArtists}</ul>
-        </div>
-      );
-    } else {
-      return <h1> Loading </h1>;
-    }
+    return <Spotify />;
   }
 }
 
