761cd062bac513ac1e270daf6846dfbecbc3a695
Grant Regimbal <regimbal.grant@gmail.com>
2018-04-06 15:33:59 -0500

    fetch inital playing song

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { spotify: false };
+  }
+  componentDidMount() {
+    axios
+      .get('/spotify')
+      .then(body => {
+        console.log(body);
+        this.setState({ spotify: body });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
-    return (
-      <h1>  World! </h1>
-    );
+    if (this.state.spotify !== false) {
+      const songArtists = this.state.spotify.data.item.artists.map((artist) =>
+        <li>{artist.name}</li>
+      );
+      const songName = this.state.spotify.data.item.name;
+      return (
+        <div>
+        <h1>{songName}</h1>
+        <ul>{songArtists}</ul>
+        </div>
+      )
+    } else {
+      return <h1> Loading </h1>;
+    }
   }
 }
 
-ReactDOM.render(<App />,  document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
